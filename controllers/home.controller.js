b437a78912a1e0b138cd149ca6172cd9d16b126f
Ayoub Moustahfid <moustahfidayoub12@gmail.com>
2020-08-22 14:51:54 +0100

    add file

---
+++ controllers/home.controller.js
@@ -0,0 +1,29 @@
+const productsModel = require("../models/products.model");
+exports.getHome = (req,res,next) =>{
+    let category = req.query.category;
+    let validCategories = ["Bijoux_Amazigh","Vêtements","Oïl_d'Argan","Babouche","Autres"];
+    let productsPromise;
+
+    if (category && validCategories.includes(category))
+        productsPromise = productsModel.getProductsByCategory(category);
+    else productsPromise = productsModel.getAllProducts();
+    productsPromise
+        .then(products => {
+            res.render("index", {
+                products: products,
+                isUser: req.session.userId,
+                isAdmin: req.session.isAdmin,
+                validationError: req.flash("validationErrors")[0],
+                pageTitle: "Home"
+            });
+        })
+        .catch(err => {
+            console.log(err);
+        });
+
+
+
+
+
+
+}
